1c714baf725ba803a6486d878f2d90342d331a4b
Easonshi <40855072+Lightblues@users.noreply.github.com>
2022-03-13 20:49:58 +0800

    fix

--- contest/260-300/268.go
+++ contest/260-300/268.go
@@ -2,8 +2,8 @@ package main
 
 import "fmt"
 
-func max(i,j int) int {
-	if i<j{
+func max(i, j int) int {
+	if i < j {
 		return j
 	}
 	return i
@@ -11,17 +11,17 @@ func max(i,j int) int {
 
 func maxDistance(colors []int) int {
 	if colors[0] != colors[len(colors)-1] {
-		return len(colors)-1
+		return len(colors) - 1
 	} else {
 		res := 0
 		currColor := colors[0]
-		for i:=0; i<len(colors);i++ {
+		for i := 0; i < len(colors); i++ {
 			if colors[i] != currColor {
-				res=max(res, len(colors)-i-1)
+				res = max(res, len(colors)-i-1)
 				break
 			}
 		}
-		for i:=len(colors)-1; i>=0;i-- {
+		for i := len(colors) - 1; i >= 0; i-- {
 			if colors[i] != currColor {
 				res = max(res, i)
 				break
@@ -30,40 +30,35 @@ func maxDistance(colors []int) int {
 		return res
 	}
 }
-func c1(){
-	fmt.Println(maxDistance([]int{1,1,1,6,1,1,1}))
+func c1() {
+	fmt.Println(maxDistance([]int{1, 1, 1, 6, 1, 1, 1}))
 }
 func wateringPlants(plants []int, capacity int) int {
-	res, remains:=0,capacity
-	for i:=0; i<len(plants); i++ {
+	res, remains := 0, capacity
+	for i := 0; i < len(plants); i++ {
 		if remains >= plants[i] {
-			res ++
+			res++
 			remains -= plants[i]
 		} else {
-			res += 2*i+1
+			res += 2*i + 1
 			remains = capacity - plants[i]
 		}
 	}
 	return res
 }
-func c2(){
-	fmt.Println(wateringPlants([]int{7,7,7,7,7,7,7}, 8))
+func c2() {
+	fmt.Println(wateringPlants([]int{7, 7, 7, 7, 7, 7, 7}, 8))
 }
 
 type RangeFreqQuery struct {
-
 }
 
-
-func Constructor(arr []int) RangeFreqQuery {
-
-}
-
+// func Constructor(arr []int) RangeFreqQuery {
+// }
 
 func (this *RangeFreqQuery) Query(left int, right int, value int) int {
 	return 0
 }
-
 
 /**
  * Your RangeFreqQuery object will be instantiated and called as such:
