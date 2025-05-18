98b6aeb64bcca538012a441c23a28449fc68bc56
easonsshi <easonsshi@tencent.com>
2025-05-18 16:00:01 +0800

    自动提交: 2025-05-18 16:00:00

--- LC-contest/401-450/429.py
+++ LC-contest/401-450/429.py
@@ -13,6 +13,7 @@ def testClass(inputs):
 
 """ 
 https://leetcode.cn/contest/weekly-contest-429
+T4 想到了二分, 但需要考虑一些边界情况!
 Easonsi @2024 """
 class Solution:
     """ 3396. 使数组元素互不相同所需的最少操作次数 """
@@ -38,7 +39,40 @@ class Solution:
                 cnt += 1
         return cnt
 
-    """ 3398. 字符相同的最短子字符串 I """
+    """ 3398. 字符相同的最短子字符串 I #hard 对于一个二进制字符串, 最多反转 ops 次, 问结果中, 最长相同子字符串的长度 
+限制: n 1e5
+思路1: #二分
+    首先, 发现不能贪心地 "修改最后一个最后一个不符合的元素". 例如, 对于 [1,1,1,0,0], ops=1 的情况下, 若目标长度为2, 则变为 [1,1,0,0,1] 发现需要2次不符合!
+    一个重要启发: 不能修改区间边缘! 
+        例如 [0,0,0,0], 对于目标长度2, 可以划分为 [0,0,1,0]
+            [0,0,0], 目标长度2, 划分为 [0,1,0]
+        这样, 对于一个长度为 l 的区间, 目标最大长度为 m, 则需要划分 l//(m+1) 次!
+    注意, 由于 "不能修改区间边缘" 的限制, 我们实际上没有考虑目标长度 =1 的情况!
+        例如 [0,0,0,1,1,0,0,0], 目标长度=1, 分区间计算的话需要3次, 但实际上最少需要4次!
+        特判! 可以暴力统计变为 [0,1,0,1...] 或 [1,0,1,0...] 的操作数
+ling: https://leetcode.cn/problems/smallest-substring-with-identical-characters-ii/solutions/3027031/er-fen-da-an-tan-xin-gou-zao-pythonjavac-3i4f/
+    """
+    def minLength(self, s: str, numOps: int) -> int:
+        n = len(s)
+        # special for 1
+        cnt = sum(int(c)==(i%2) for i,c in enumerate(s))
+        if min(cnt, n-cnt) <= numOps: return 1
+        # binary search
+        lens = []; l = -1
+        for i,c in enumerate(s):
+            if i==n-1 or c!=s[i+1]:
+                lens.append(i-l)
+                l = i
+        def check(m):
+            return sum(l//(m+1) for l in lens) <= numOps
+        l,r = 2,n; ans = n
+        while l<=r:
+            m = (l+r)//2
+            if check(m):
+                ans = m
+                r = m-1
+            else: l = m+1
+        return ans
     
     
 
@@ -46,7 +80,9 @@ class Solution:
 sol = Solution()
 result = [
     # sol.minimumOperations(nums = [1,2,3,4,2,3,3,5,7]),
-    sol.maxDistinctElements(nums = [4,4,4,4], k = 1),
+    # sol.maxDistinctElements(nums = [4,4,4,4], k = 1),
+    sol.minLength(s = "000001", numOps = 1),
+    sol.minLength(s = "0000", numOps = 2),
 ]
 for r in result:
     print(r)
--- LC-contest/d101-150/d139.py
+++ LC-contest/d101-150/d139.py
@@ -1,4 +1,5 @@
 from typing import *
+from heapq import heappop, heappush
 # from easonsi.util.leetcode import *
 
 # def testClass(inputs):
@@ -13,20 +14,46 @@ from typing import *
 
 """ 
 https://leetcode.cn/contest/biweekly-contest-139
+TODO: T3/4
 Easonsi @2025 """
 class Solution:
-    """  """
+    """ 3285. 找到稳定山的下标 """
+    def stableMountains(self, height: List[int], threshold: int) -> List[int]:
+        ans = []
+        for i in range(1, len(height)):
+            if height[i-1] > threshold:
+                ans.append(i)
+        return ans
     
-    
-    
-    
+    """ 3286. 穿越网格图的安全路径 #medium 
+经典 #Dijkstra 的题目, 
+但这里边权是 0/1, 因此可以用  0-1 BFS!
+    """
+    def findSafeWalk(self, grid: List[List[int]], health: int) -> bool:
+        m,n = len(grid), len(grid[0])
+        vis = [(0,0)]
+        frontier = [(grid[0][0], 0,0)]
+        while frontier:
+            d,i,j = heappop(frontier)
+            for di, dj in [(1,0), (-1,0), (0,1), (0,-1)]:
+                if i==m-1 and j==n-1:  # reach the target!
+                    return True
+                x,y = i+di, j+dj
+                if x<0 or x>=m or y<0 or y>=n: continue
+                if (x,y) in vis: continue
+                v = d + grid[x][y]
+                if v >= health: continue
+                vis.append((x,y))
+                heappush(frontier, (v, x, y))
+        return False
     
     
 
     
 sol = Solution()
 result = [
-    
+    sol.findSafeWalk(grid = [[1,1,1],[1,0,1],[1,1,1]], health = 5),
+    sol.findSafeWalk(grid = [[0,1,1,0,0,0],[1,0,1,0,0,0],[0,1,1,1,0,1],[0,0,1,0,1,0]], health = 3),
 ]
 for r in result:
     print(r)
