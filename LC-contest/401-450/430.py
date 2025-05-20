8f194e2789ee1a451402e8a7173bc5757290c85d
easonsshi <easonsshi@tencent.com>
2025-05-20 10:00:00 +0800

    自动提交: 2025-05-20 10:00:00

--- LC-contest/401-450/430.py
+++ LC-contest/401-450/430.py
@@ -46,26 +46,45 @@ class Solution:
     
     # TODO:T3, T4
     """ 1163. 按字典序排在最后的子串 #hard 对于一个字符串的所有子串, 找到字典序最大的子串
-思路1: 
+思路1: #双指针 -- 这里的核心是递推!
     显然, 结果一个是一个 "后缀子串"
     #双指针 其中 i 表示当前最大子串的左端点, j 表示目前枚举的左端点. 
     假设这两个子串的公共前缀为 k-1, 对于 s[i+k], s[j+k],
-    若 s[i+k] > s[j+k]
-        若 i+k <= j, 则下一个枚举 j+1 (平凡的情况)
-        若 i+k > j, 我们要证明 j...j+k 都不需要枚举! (下一个枚举 j+k+1)
-            可以将其拆成 i+m <= j; i+m > j 两部分
-            对于 i+m <= j, 显然有 s[j+m:] < s[i+m:] < s[i:] -- 其中第一个不等式是因为 s[i+k] > s[j+k]; 第二个不等式是沿用递推的结论
     若 s[i+k] < s[j+k]
+        显然要更新 i=j, 对于 j,
+        若 i+k <= j, 则下一个枚举 j+1 (平凡的情况)
+        若 i+k > j, 我们要证明 i...i+k 都不需要枚举! (下一个枚举 i+k+1)
+            对于 i...j 的部分之前考虑过了! 对于 j+1...i+k, 考虑拆成 i+m+(j-i) <= i+k, i+m+(j-i) > i+k 两部分
+            - 对于后者, 也即 m > k-(j-i), 有 s[i+m:] < s[j+m:] 这个是因为一开始的分类条件, 而 j+m > i+k, 我们之后会考虑!
+            - 对于前者, 也即 m <= k-(j-i), 因为我们总有 s[i+m] < s[j+m], 也即将考虑的坐标加上了 j-i, 因为连续不等式总会将考虑的坐标放在后续比较的范围内, 因此也不需要考虑! (转为情况1)
+            综上, 下一个要枚举的对象为 i+k+1
+    若 s[i+k] > s[j+k]:
+        我们要证明 j...j+k 都不需要枚举! (下一个枚举 j+k+1)
+            类似上面, 我们同样有 s[j+m] < s[j-(j-i)+m:] < ... 因为总能将比较的对象转为我们已经考虑过的情况!
+    复杂度: O(n)
+参考下面的图!
 https://leetcode.cn/problems/last-substring-in-lexicographical-order/solutions/2241014/an-zi-dian-xu-pai-zai-zui-hou-de-zi-chua-31yl/
     """
     def lastSubstring(self, s: str) -> str:
         n = len(s)
-        
+        i,j = 0,1
+        while j<n:
+            k = 0
+            while j+k<n and s[i+k]==s[j+k]:
+                k += 1
+            if j+k<n and s[i+k]<s[j+k]:
+                i = j
+                j = max(j+1, i+k+1)
+            else:
+                j = j+k+1
+        return s[i:]
 
     
 sol = Solution()
 result = [
-    sol.answerString(word = "dbca", numFriends = 2),
+    # sol.answerString(word = "dbca", numFriends = 2),
+
+    sol.lastSubstring(s = "abab"),
 ]
 for r in result:
     print(r)
