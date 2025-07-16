4d18469fb14121315509927d4291d7f5c9931909
easonsshi <easonsshi@tencent.com>
2025-07-17 00:00:00 +0800

    Auto commit: 2025-07-17 00:00:00

--- LC-contest/d101-150/d144.py
+++ LC-contest/d101-150/d144.py
@@ -5,11 +5,20 @@ https://leetcode.cn/contest/biweekly-contest-144
 
 Easonsi @2025 """
 class Solution:
-    """  """
+    """ 3360. 移除石头游戏 """
+    def canAliceWin(self, n: int) -> bool:
+        t = 10
+        for i in range(10):
+            if n >= t:
+                n -= t
+                t -= 1
+            else:
+                return i % 2 == 1
 
 sol = Solution()
 result = [
-
+    sol.canAliceWin(n = 12),
+    sol.canAliceWin(2),
 ]
 for r in result:
     print(r)
