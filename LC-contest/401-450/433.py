504156894d62e8d2e68d282ba4e37c7825951fae
easonsshi <easonsshi@tencent.com>
2025-09-01 23:00:00 +0800

    Auto commit: 2025-09-01 23:00:00

--- LC-contest/401-450/433.py
+++ LC-contest/401-450/433.py
@@ -1,23 +1,33 @@
+from itertools import accumulate
 from typing import *
 
 
 """ 
-https://leetcode.cn/contest/weekly-contest-431
+https://leetcode.cn/contest/weekly-contest-433
 
 Easonsi @2025 """
 class Solution:
-    """  """
+    """ 3427. 变长子数组求和 """
+    def subarraySum(self, nums: List[int]) -> int:
+        acc = list(accumulate(nums, initial=0))
+        ans = 0
+        for i,x in enumerate(nums):
+            start = max(0, i-x)
+            ans += acc[i+1] - acc[start]
+        return ans
     
-    
-    
-    
+    """ 3428. 最多 K 个元素的子序列的最值之和 """
+    def minMaxSums(self, nums: List[int], k: int) -> int:
+        
+        nums.sort()
+
     
     
 
     
 sol = Solution()
 result = [
-    
+    sol.subarraySum(nums = [2,3,1]),
 ]
 for r in result:
     print(r)
