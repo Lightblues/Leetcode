354b5f4693b639fe21f861fbfcff77b341c88240
easonsshi <easonsshi@tencent.com>
2025-09-11 15:00:00 +0800

    Auto commit: 2025-09-11 15:00:00

--- LC-contest/401-450/435.py
+++ LC-contest/401-450/435.py
@@ -1,15 +1,23 @@
 from typing import *
-
+from collections import Counter
+from math import inf
 
 """ 
-https://leetcode.cn/contest/weekly-contest-431
+https://leetcode.cn/contest/weekly-contest-435
 
 Easonsi @2025 """
 class Solution:
-    """  """
+    """ 3442. 奇偶频次间的最大差值 I """
+    def maxDifference(self, s: str) -> int:
+        mx, mn = -inf, inf
+        for freq in Counter(s).values():
+            if freq % 2 == 1:
+                mx = max(mx, freq)
+            else:
+                mn = min(mn, freq)
+        return mx - mn
     
-    
-    
+    """ 3443. K 次修改后的最大曼哈顿距离 """
     
     
     
@@ -17,7 +25,7 @@ class Solution:
     
 sol = Solution()
 result = [
-    
+    sol.maxDifference(s = "aaaaabbc"),
 ]
 for r in result:
     print(r)
