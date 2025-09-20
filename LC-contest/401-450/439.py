1be5b433a1ffb3cc37be99797a78c8ce02afaafe
Lightblues <oldcitystal@gmail.com>
2025-09-21 06:01:12 +0800

    Auto commit: 2025-09-21 06:01:11

--- LC-contest/401-450/439.py
+++ LC-contest/401-450/439.py
@@ -19,31 +19,14 @@ https://leetcode.cn/problems/find-the-largest-almost-missing-integer/solutions/3
         if nums[-1] not in nums[:-1]: ans = max(ans, nums[-1])
         return ans
     
-    """ 3472. 至多 K 次操作后的最长回文子序列 #medium 每次操作可以把一个字符替换为相邻字符 (z可以变为a), 问k次操作后得到的最长回文子串
+    """ 3472. 至多 K 次操作后的最长回文子序列 #medium 每次操作可以把一个字符替换为相邻字符 (z可以变为a), 问k次操作后得到的最长回文子序列
 限制: n 200; k 200
+注意理解题意! 要求的是 "子序列"
     """
     def longestPalindromicSubsequence(self, s: str, k: int) -> int:
         s = [ord(c)-ord('a') for c in s]
         n = len(s)
-        ans = 1
-        def calc_d(l,r):
-            return min(abs(l-r), 26-abs(l-r))
-        for center in range(1,n-1):
-            remain = k
-            for i in range(1, min(center+1, n-center)):
-                l,r = s[center-i], s[center+i]
-                d = calc_d(l,r)
-                if d > remain: break
-                remain -= d
-                ans = max(ans, 2*i+1)
-            remain = k - calc_d(s[center], s[center+1])
-            for i in range(1, min(center+1, n-center-1)):
-                l,r = s[center-i], s[center+i+1]
-                d = calc_d(l,r)
-                if d > remain: break
-                remain -= d
-                ans = max(ans, 2*i+2)
-        return ans
+
     
     
 
