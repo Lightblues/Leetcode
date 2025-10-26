b81ddd168c01a509e2e3da14bbea1348e95746c7
Lightblues <oldcitystal@gmail.com>
2025-10-26 10:00:01 +0800

    Auto commit: 2025-10-26 10:00:00

--- LC-contest/401-450/445.py
+++ LC-contest/401-450/445.py
@@ -1,13 +1,25 @@
 from typing import *
 import heapq
+from collections import Counter
 
 """ 
-https://leetcode.cn/contest/weekly-contest-441
+https://leetcode.cn/contest/weekly-contest-445
 
 Easonsi @2025 """
 class Solution:
-    """  """
-    
+    """ 3516. 找到最近的人 """
+    def findClosest(self, x: int, y: int, z: int) -> int:
+        a = abs(x-z)
+        b = abs(y-z)
+        if a < b: return 1
+        elif a > b: return 2
+        else: return 0
+
+    """ 3517. 最小回文排列 I """
+    def smallestPalindrome(self, s: str) -> str:
+        cnt = Counter(s)
+
+
 sol = Solution()
 result = [
     
