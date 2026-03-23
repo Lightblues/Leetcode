832f35479fc6886b1a2086a854a1ff5df38f1069
Lightblues <oldcitystal@gmail.com>
2026-03-23 21:00:01 +0800

    Auto commit: 2026-03-23 21:00:01

--- LC-contest/401-450/446.py
+++ LC-contest/401-450/446.py
@@ -5,17 +5,48 @@ https://leetcode.cn/contest/weekly-contest-446
 Easonsi @2025 """
 class Solution:
     """  """
-    
-    
-    
-    
-    
-    
+    def calculateScore(self, instructions: List[str], values: List[int]) -> int:
+        n = len(instructions)
+        score = 0
+        visited = set()
+        i = 0
+        
+        while i >= 0 and i < n:
+            if i in visited:
+                break
+            visited.add(i)
+            
+            if instructions[i] == "add":
+                score += values[i]
+                i += 1
+            else:  # "jump"
+                i += values[i]
+        
+        return score
 
-    
+    def maximumPossibleSize(self, nums: List[int]) -> int:
+        n = len(nums)
+        ans = 0
+        prev_max = 0  # 上一段的 max
+        cur_max = 0   # 当前段的 max
+        for i in range(n):
+            cur_max = max(cur_max, nums[i])
+            # 当前段的 max >= 上一段的 max，满足非递减，可以在此结束当前段
+            if cur_max >= prev_max:
+                ans += 1
+                prev_max = cur_max
+                cur_max = 0
+        # 如果最后还剩余元素（cur_max > 0 说明最后一段还没结束），
+        # 需要将其合并到前一段（不增加 ans）
+        return ans
+
+
 sol = Solution()
 result = [
-    
+    sol.calculateScore(instructions = ["jump","add","add","jump","add","jump"], values = [2,1,3,1,-2,-3]),
+    sol.maximumPossibleSize(nums = [4,2,5,3,5]),
+    sol.maximumPossibleSize(nums = [1,2,3]),
+    sol.maximumPossibleSize(nums = [1,1,1]),
 ]
 for r in result:
     print(r)
--- LC-contest/401-450/447.py
+++ LC-contest/401-450/447.py
@@ -1,21 +1,51 @@
 from typing import *
 
 """ 
-https://leetcode.cn/contest/weekly-contest-446
+https://leetcode.cn/contest/weekly-contest-447
 Easonsi @2025 """
 class Solution:
-    """  """
-    
-    
-    
-    
-    
-    
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        # 对每一行/每一列, 记录该行/列上建筑坐标的最小值和最大值
+        # 一个建筑 (x, y) 被覆盖 ⟺
+        #   同一列(x相同)存在 y' < y 和 y'' > y  (上下方向)
+        #   同一行(y相同)存在 x' < x 和 x'' > x  (左右方向)
+        # 等价于: x 严格在该列 x 范围内部, y 严格在该行 y 范围内部
+        row_min = {}  # row -> (min_x, max_x)  同一行 y 相同, 记录 x 的范围
+        col_min = {}  # col -> (min_y, max_y)  同一列 x 相同, 记录 y 的范围
+        for x, y in buildings:
+            if y not in row_min:
+                row_min[y] = (x, x)
+            else:
+                row_min[y] = (min(row_min[y][0], x), max(row_min[y][1], x))
+            if x not in col_min:
+                col_min[x] = (y, y)
+            else:
+                col_min[x] = (min(col_min[x][0], y), max(col_min[x][1], y))
+        ans = 0
+        for x, y in buildings:
+            # 左右: 同行(y相同)中, x 严格在 (min_x, max_x) 内部
+            # 上下: 同列(x相同)中, y 严格在 (min_y, max_y) 内部
+            rmin, rmax = row_min[y]
+            cmin, cmax = col_min[x]
+            if rmin < x < rmax and cmin < y < cmax:
+                ans += 1
+        return ans
 
-    
+
+    def pathExistenceQueries(self, n: int, nums: List[int], maxDiff: int, queries: List[List[int]]) -> List[bool]:
+        # nums 已按非递减排序, 相邻元素差 <= maxDiff 则连通
+        # 扫描一遍, 给每个节点分配连通分量编号
+        comp = [0] * n
+        for i in range(1, n):
+            if nums[i] - nums[i - 1] > maxDiff:
+                comp[i] = comp[i - 1] + 1
+            else:
+                comp[i] = comp[i - 1]
+        return [comp[u] == comp[v] for u, v in queries]
+
+# 测试用例
 sol = Solution()
 result = [
-    
 ]
 for r in result:
     print(r)
