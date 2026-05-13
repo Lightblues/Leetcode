b917898a9b1216b023b5ffc6db632d37de3f8e5c
easonsshi <easonsshi@tencent.com>
2026-05-13 23:04:08 +0800

    lc 456,7

--- LC-contest/451-550/456.py
+++ LC-contest/451-550/456.py
@@ -1,17 +1,226 @@
 from typing import *
+from functools import cache
+import math
+import itertools
+import operator
+import bisect
+import heapq
 
 """
 https://leetcode.cn/contest/weekly-contest-456
+1. 关于字符串哈希的题目
+    注意字符串 hash 的复杂度; 
+    另外字典树的实现也值得借鉴!
+2. 对于一个数组, 计算去除任意位置元素后的影响
+    "维护前二大 LCP" 的思路 & 代码技巧
+    这类问题 "前后缀分解" 的思路!
+3. 最小化最大值问题 
+    1. 划分型DP 属于 "约束划分个数" 的题型
+    2. 这类问题另一类想法: 二分! -- 不过还需要建图求解
+    3. 更完全地建模为图上搜索问题 -- Dijkstra
+4. 最大化生成树边权问题, 核心是对于 生成树 的理解 -- 通过并查集来检查!
 Easonsi @2026 """
 
 
 class Solution:
-    """  """
+    """ 3597. 分割字符串 给定一个字符串进行分段. 规则是记录所有已出现的段, 思路出现一个新段 
+限制: n 1e5
+思路 1: #哈希集合
+    复杂度: NOTE 字符串 #hash 的复杂度为 O(L)! -- 计算 hash 的操作
+        因此, 考虑构成段的最大长度, 可知在所有字符都相同的情况下最大为 1,2,... -> 因此最大长度为 log n
+        考虑遍历操作, 整体复杂度 O(n logn)
+思路 2: #字典树 #trie
+    复杂度: O(n) -- 这里是用 hash 来实现的; 如果是列表来存储 (而非字典) 复杂度为 O(n|Σ|)
+https://leetcode.cn/problems/partition-string/solutions/3710991/an-ti-yi-mo-ni-ha-xi-ji-he-pythonjavacgo-p761/
+    """
+    def partitionString(self, s: str) -> List[str]:
+        st = set()
+        ans = []
+        l = 0
+        for i,ch in enumerate(s):
+            if s[l:i+1] not in st:  # NOTE: 这里可以通过维护一个递增的字符串来避免字符串切分!
+                ans.append(s[l:i+1])
+                st.add(s[l:i+1])
+                l = i+1
+        return ans
 
+    def partitionString(self, s: str) -> List[str]:
+        curr = root = {}
+        ans = []
+        l = 0
+        for i, c in enumerate(s):
+            if c not in curr:
+                curr[c] = {}
+                ans.append(s[l:i+1])
+                curr = root
+                l = i+1
+            else: curr = curr[c]
+        return ans
+
+    """ 3598. 相邻字符串之间的最长公共前缀 #medium
+对于一个字符串数字, 计算移除每一个i 位置 word 后, "相邻字符串的最长前缀的最大值"
+限制: n 1e5; L 1e4
+思路 1: 维护前三大 LCP
+    对于原数组, 可以计算所有相邻位置的 #LCP. 对于一次移除操作, 减少两个 lcp, 新增一个 -- 考虑这些变化即可!
+        在代码实现上, 只需要位置 mx1,mx2,mx3, i1,i2 即可! 其中 i1/i2 标记最大/次大出现的位置, 在 for 循环中可以简化判断!
+        - 若移除元素 i 影响了 mx1,mx2, 答案为 max(mx3, l); -- 其中 l 为新增那个 lcp
+        - 若只影响 mx1, 答案为 max(mx2, l);
+        - 否则, 答案为 max(mx1, l);
+    复杂度: O(L), 其中 L 为所有 word 长度之和
+    ling 提供了优雅得多的代码!
+思路 2: 维护前二大 LCP
+    对于上面继续优化! 考虑 "同时影响 mx1,mx2" 的情况, 只可能出现在 i1,i2 相邻的时候!
+    有定理: 此时 l >= mx2. 因此直接返回 l 即可!
+        直接考虑 (A, B, C), 假设 lcp 分别为 mx1, mx2, 可知 lcp(A,C)>=mx2
+思路 3: #前后缀分解
+    对于位置 i, 问题等价于: max{ max(0..i-1), lcp(i-1,i+1), max(i+1...n-1) }
+    计算 "前后缀" 最大值, 可以直接 #递推 计算
+    复杂度: O(L)
+https://leetcode.cn/problems/longest-common-prefix-between-adjacent-strings-after-removals/solutions/3710963/qian-hou-zhui-fen-jie-pythonjavacgo-by-e-8sn2/
+    """
+    def longestCommonPrefix(self, words: List[str]) -> List[int]:
+        if len(words) <= 2: return [0] * len(words)  # NOTE: 注意边界!
+        def check(s: str, t: str) -> int:
+            for i, (a,b) in enumerate(zip(s,t)):
+                if a!=b: return i
+            return min(len(s), len(t))
+        n = len(words)
+        dist = [check(words[i], words[i+1]) for i in range(n-1)]
+        dist_max = sorted(dist, reverse=True)[:3]
+        ans = []
+        for i in range(n):
+            dist_max_ = dist_max[:]
+            if i-1>=0 and dist[i-1] in dist_max_: dist_max_.remove(dist[i-1])
+            if i<n-1 and dist[i] in dist_max_: dist_max_.remove(dist[i])
+            if i-1>=0 and i+1<n: dist_max_.append(check(words[i-1], words[i+1]))
+            ans.append(max(dist_max_))
+        return ans
+
+    def longestCommonPrefix(self, words: List[str]) -> List[int]:
+        # 思路 3: #前后缀分解
+        @cache
+        def lcp(s: str, t: str) -> int:
+            cnt = 0
+            for a,b in zip(s,t):
+                if a==b: cnt += 1
+                else: break
+            return cnt
+
+        n = len(words)
+        if n == 1: return [0]
+        suf_max = [0] * n  # words[i:] 的后缀最大值
+        for i in range(n-2, -1, -1):
+            suf_max[i] = max(suf_max[i+1], lcp(words[i], words[i+1]))
+        ans = [0] * n
+        ans[0] = suf_max[1]  # 第一个对应位置为 1
+        pre_max = 0
+        for i in range(1,n-1):  # 注意边界!
+            ans[i] = max(pre_max, lcp(words[i-1],words[i+1]), suf_max[i+1])
+            pre_max = max(pre_max, lcp(words[i-1], words[i]))
+        ans[-1] = pre_max  # 只计算到了 n-2!
+        return ans
+
+    """ 3599. 划分数组得到最小 XOR
+要求将数组划分为 k 段, 要求 min{ max(XOR(seg)) }
+限制: n,k 250
+思路 1: #划分型DP 属于 "约束划分个数" 的题型
+    记 f(i,j) 为对于前缀 i 拆分成j 段的最小值, 则有 f(i,j) = min{ max(f(k-1,j-1), xor(k..i)) }
+    复杂度: O(k(n-k)^2) 平方项是 n-1,...,n-k+1 枚举构成! 
+    优化: 下面 NOTE 里面的两点, 参见 ling
+思路 2: 二分 (但二分里面还是得写 DP, 没必要)
+    考虑二分: 则问题变为, 在上界 upper 限制, 对于所有的 (i,j), 若 xor(i+1...j) <= upper 则连边, 问是否有从 0 -> n 的路径, 恰好 k 步
+    定义 dfs (i) 为 从 i 到 0 的所有可能的步数集合, 例如若从 5 到 0 可以走 1/3/4 步, 则 dfs(5) = {1,3,4}
+    边界: dfs(0) = {0}
+    入口: dfs(n) -> 答案就是判断其中是否包含 k
+    #技巧: 为计算区域 xor, 有 xor(i+1...j) = pre_xor(i) ^ pre_xor(j)
+    复杂度: O(n^3/w logU), 其中 U 为搜索范围; n^2 为搜索的复杂度; O(n/w) 是处理位长 n 的二进制集合表示的复杂度!
+思路 3: 采用 #Dijkstra
+    考虑从 (n,k) -> (0,0) "最短路" -- 其中最短的定义不是求和而是 max 操作
+    建图方式同上，边权为子数组异或和
+    复杂度: O(M logM), 其中边数 M = n^2k
+https://leetcode.cn/problems/partition-array-to-minimize-xor/solutions/3710966/hua-fen-xing-dp-de-tong-yong-tao-lu-pyth-lmcm/
+    """
+    def minXor(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        # 初始化 j=1
+        f = []; xor_ = 0
+        for x in nums:  # NOTE: 可以合并到下面 for 中
+            xor_ ^= x
+            f.append(xor_)
+        for j in range(2,k+1):
+            nf = [-1] * n  # NOTE: 可以倒序原地更新, 不用开新数组
+            for i in range(j-1,n):
+                mn = math.inf
+                xor_ = 0
+                for k in range(i, j-2, -1):
+                    xor_ ^= nums[k]
+                    mn = min(mn, max(f[k-1], xor_))
+                nf[i] = mn
+            f = nf
+        return f[-1]
+
+    def minXor(self, nums: List[int], k: int) -> int:
+        s = list(itertools.accumulate(nums, operator.xor, initial=0))  # 前缀异或和; accumulate 包括前缀 0
+
+        def check(upper: int) -> bool:
+            @cache
+            def dfs(i: int) -> int:
+                if i==0: return 1
+                res = 0
+                for j in range(i):
+                    if s[i] ^ s[j] <=upper:
+                        res |= dfs(j) << 1  # 位运算复杂度: O(n/w)
+                return res
+            return dfs(len(nums)) >> k & 1 > 0
+
+        mx = (1 << max(nums).bit_length()) - 1  # 二分最大: 看最大数字有多少位!
+        # NOTE: 位运算优先级!
+        return bisect.bisect_left(range(mx), True, key=check) # NOTE: bisect 用法
+
+    def minXor(self, nums: List[int], k: int) -> int:
+        max = lambda a, b: b if b > a else a
+        s = list(itertools.accumulate(nums, operator.xor, initial=0))  # 前缀异或和; accumulate 包括前缀 0
+        n = len(nums)
+
+        dist = [[math.inf] * (k+1) for _ in range(n+1)]
+        h = [(0, n, k)]
+        while h:
+            d, i, k_ = heapq.heappop(h)
+            if d > dist[i][k_]: continue  # 已有更优的达到 (i,k_) 的方案
+            if k_==0:  # 走完了 k 步
+                if i==0: return d
+                continue
+            for j in range(i):
+                nd = max(s[i] ^ s[j], d)
+                if nd < dist[j][k_-1]:
+                    dist[j][k_-1] = nd
+                    heapq.heappush(h, (nd, j, k_-1))
+        # NOTE: 题目保证了一定有答案 (k<=n)
+
+    """ 3600. 升级后最大生成树稳定性
+给定一组边 (u,v,s,must), s 是初始的强度; must=1 表示必须出现在生成树中, 且不能升级; =0 表示可升级一次 s*=2
+给定最多k 次升级, 求 max{生成树稳定性}, "稳定性" 为生成树的最小边权
+限制: n 1e5
+思路 1: 二分答案 + 并查集
+    处理边界情况: 若本身不联通, 或者 must 构成了环, 返回 -1, 否则一定有值
+    确保有解的情况下, 采用 #二分
+        对于每个 low 做 check:
+            首先, 处理所有 must=1 (若 s<low 直接不可行) 和 s>=lower 的边 -- 利用并查集合并
+            然后, 依次遍历所有剩余边, 若 2s>=low, 说明可以连接两个 component, 加入! 验证在 k 范围内是否可以做到
+                NOTE: 只需要遍历即可! 生成树特性!
+    复杂度: O((n+mlogn) logU) 其中m 为边数; logU 是二分复杂度.
+思路 2: 可以用 #Kruskal 算法 来求 #最大生成树
+    """
+    def maxStability(self, n: int, edges: List[List[int]], k: int) -> int:
+        pass
 
 sol = Solution()
 result = [
-
+    # sol.partitionString(s = "abbccccd"),
+    # sol.partitionString("aaaa"),
+    # sol.longestCommonPrefix(["jump","run","run","jump","run"]),
+    sol.minXor(nums = [1,2,3], k = 2),
+    sol.minXor(nums = [2,3,3,2], k = 3),
 ]
 for r in result:
     print(r)
--- LC-contest/451-550/457.py
+++ LC-contest/451-550/457.py
@@ -1,17 +1,233 @@
 from typing import *
+import string
+import math
+from heapq import heapify, heappop, heappush
+from collections import defaultdict
+from sortedcontainers import SortedList
 
 """
 https://leetcode.cn/contest/weekly-contest-457
+1. 组合考察排序 & 字符串操作
+2. 网络分块 + 维护区块最小值
+    - 分块: 除了用 1. 并查集; 2. DFS 的好处是天然构建了序号! 更为简单
+    - 维护固定区块的 active 集最小值
+        1. 通过倒序来不断加入 -- 只需要不断 min
+        2. 懒删除堆! 增加的复杂度很低
+3. 计算联通分量的变化 -- 标准 并查集
+4. 规定行动方式的格点跳转 -- 逆向思维 (本质上就理清条件!)
 Easonsi @2026 """
 
 
 class Solution:
-    """  """
+    """ 3606. 优惠券校验器 
+https://leetcode.cn/problems/coupon-code-validator/solutions/3716401/mo-ni-pythonjavacgo-by-endlesscheng-xqv4/
+"""
+    def validateCoupons(self, code: List[str], businessLine: List[str], isActive: List[bool]) -> List[str]:
+        s_valid_code = set(string.ascii_letters) | set(string.digits) | set(["_"])
+        valid = []
+        for c,b,a in zip(code, businessLine, isActive):
+            if not a: continue
+            if b not in ("electronics", "grocery", "pharmacy", "restaurant"): continue
+            if c and set(c).issubset(s_valid_code): valid.append((b, c))
+        valid.sort()
+        return [v[1] for v in valid]
 
+    """ 3607. 电网维护
+对于 c 个节点 n 条边的电网, 执行若干操作: 1. 检查 x 站, 若在线则返回; 否则返回其所在网络中在线的最小那个; 2. 将 x 离线.
+限制: c,n 1e5; q 2e5
+分解为两部分: 维护电网关系; 检查最小在线设备
+- 构建连接关系:
+    思路 1: #并查集 Union-Find / Disjoint Set Union
+    思路 2: 直接建图 DFS
+- 检查最小在线
+    思路 1: 暴力 #有序数组 复杂度 O(q logn)
+    思路 2: #倒序 维护每个group里面最小的在线设备
+        注意: 一台可以被多次下线, 需要先正序求最早在线状态
+        复杂度: O(q)
+    思路 3: #懒删除堆
+        复杂度: O(q logc); 另外 heapify 操作可能是 c 或者 c logc
+https://leetcode.cn/problems/power-grid-maintenance/solutions/3716402/dfs-lan-shan-chu-dui-pythonjavacgo-by-en-17gb/
+"""
+    def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
+        # 并查集
+        fa = list(range(c+1))
+        def find(x: int) -> int:
+            # find the root
+            root = x
+            while fa[root] != root:
+                root = fa[root]
+            # update (路径压缩)
+            while fa[x] != x:
+                fa[x], x = root, fa[x]
+            return root
+        def join(x: int, y: int):
+            fa[find(x)] = find(y)
+        for x,y in connections:
+            join(x,y)
+        # 暴力 #有序数组
+        # x_to_g = {}
+        # g_sl = defaultdict(SortedList)
+        # for x in range(1, c+1):
+        #     g = find(x)
+        #     g_sl[g].add(x)
+        #     x_to_g[x] = g
+        # ans = []
+        # offline_s = set()
+        # for op, x in queries:
+        #     if op==1:
+        #         if x not in offline_s: ans.append(x)
+        #         elif len(g_sl[x_to_g[x]]): ans.append(g_sl[x_to_g[x]][0])
+        #         else: ans.append(-1)
+        #     else:
+        #         offline_s.add(x)
+        #         g_sl[x_to_g[x]].discard(x)
+        # return ans
+
+        # 2. 逆序
+        x_to_g = [-1] * (c+1)
+        for x in range(1, c+1):
+            x_to_g[x] = find(x)
+        first_offline = [math.inf] * (c+1)  # 记录每个节点的离线时间，初始为无穷大（始终在线
+        for i,(op,x) in enumerate(queries):
+            if op==2 and first_offline[x] == math.inf: 
+                first_offline[x] = i
+        ans = []
+        g_min = defaultdict(lambda: math.inf)  # 每组中的最小在线号数
+        for i in range(1, c+1):
+            if first_offline[i] == math.inf:
+                g = x_to_g[i]
+                g_min[g] = min(g_min[g], i)
+        for i in range(len(queries)-1, -1,-1):
+            op,x = queries[i]
+            g = x_to_g[x]
+            if op==2:
+                if i==first_offline[x]: g_min[g] = min(g_min[g], x)
+            else:
+                if i < first_offline[x]: ans.append(x)
+                elif g_min[g] != math.inf: ans.append(g_min[g])
+                else: ans.append(-1)
+        return ans[::-1]
+
+    def processQueries(self, c: int, connections: List[List[int]], queries: List[List[int]]) -> List[int]:
+        # 基于 DFS 分组!
+        g = [[] for _ in range(c+1)]
+        for x,y in connections:
+            g[x].append(y)
+            g[y].append(x)
+        belong = [-1] * (c+1)
+        heaps = []  # 记录每个组包含的节点, heap
+        def dfs(x: int) -> None:
+            belong[x] = len(heaps)  # 记录节点 x 在哪个堆
+            h.append(x)  # NOTE: 这里的 h 在外层初始化!
+            for y in g[x]:
+                if belong[y] == -1:
+                    dfs(y)
+        for x in range(1, c+1):
+            if belong[x] != -1: continue
+            h = []
+            dfs(x)
+            heapify(h)
+            heaps.append(h)
+        # 3. 懒删除堆
+        ans = []
+        offline = [False] * (c+1)
+        for op,x in queries:
+            if op==2: offline[x] = True
+            else:
+                if not offline[x]: ans.append(x)
+                else:
+                    h = heaps[belong[x]]
+                    while h and offline[h[0]]: heappop(h)  # 
+                    ans.append(h[0] if h else -1)  # 简写
+        return ans
+
+    """ 3608. 包含 K 个连通分量需要的最小时间 
+一个图的每条边会在 t_i 时间消失, 求构成至少 k 个连通分量的最小时间
+限制: n 1e5
+思路 1: #并查集 逆向考虑
+    复杂度: O(n+mlogm+mlogn)
+https://leetcode.cn/problems/minimum-time-for-k-connected-components/solutions/3716407/bing-cha-ji-cong-da-dao-xiao-he-bing-pyt-03qz/
+    """
+    def minTime(self, n: int, edges: List[List[int]], k: int) -> int:
+        edges.sort(key=lambda x: -x[2]) # 按照 time 降序排列
+        uf = UnionFind(n)
+        for u,v,t in edges:
+            uf.merge(u,v)
+            if uf.cc < k:   # 这条边不能留，即移除所有 time <= t 的边
+                return t
+        return 0 # 无需移除任何边
+
+    """ 3609. 到达目标点的最小移动次数 
+从 (sx, sy) -> (tx, ty). 对于 (x,y), 记 m = max(x,y) 每次移动可以是 (x+m, y) or (x,y+m). 求最小移动步数
+限制: v 1e9 >0
+思路 1: #逆向思维 + 分类讨论
+    反向, 考虑从 (x,y) 走到 (sx, sy)
+    - 不失一般性, 假设 x>=y 且 x>0 (对于 x=y=0 的情况无需考虑; 结论有对称性)
+    分类考虑上一步的操作:
+        (x/2, y). 则要求 x/2>=y, x>=2y 且为偶数
+        (x-y, y). 则要求 x-y<=y, y<=x<=2y
+        (x,y/2). 则要求 x<=y/2 和假设矛盾
+        (x,y-x). 由于不考虑负数, 因此假设了 y-x>=0, 也即 x=y>0, 倒推操作后一个变为 0
+    边界:
+        x=sx & y=sy -> 返回
+        x<sx or y<sy -> -1
+    现在考虑倒推的过程:
+        若 x>=2y: 奇数不可能; 偶数 //2
+        若 x>y: x -= y
+        若 x==y: 此时两种操作
+    复杂度: O(log(tx+ty))
+https://leetcode.cn/problems/minimum-moves-to-reach-target-in-grid/solutions/3716440/ni-xiang-si-wei-fen-lei-tao-lun-yan-ge-z-m5cc/
+    """
+    def minMoves(self, sx: int, sy: int, tx: int, ty: int) -> int:
+        x,y = tx,ty
+        cnt = 0
+        while x!=sx or y!=sy:  # NOTE: 注意终止条件!
+            if x<sx or y<sy:  # 失败
+                return -1
+            cnt += 1
+            # 下面直接枚举了所有可能情况; 可以参考 ling 的讨论, 通过调换 sx,sy 来维护 x>=y 的条件
+            if x>=2*y:
+                if x%2: return -1
+                x //= 2
+            elif y>= 2*x:
+                if y%2: return -1
+                y //= 2
+            elif x > y:
+                x -= y
+            elif y > x:
+                y -= x
+            else:
+                if sx==0: x=0
+                else: y=0
+        return cnt
+
+class UnionFind:
+    def __init__(self, n: int):
+        self.fa = list(range(n))
+        self.cc = n
+    # 返回 x 所在集合的代表元
+    # 同时做路径压缩，也就是把 x 所在集合中的所有元素的 fa 都改成代表元
+    def find(self, x: int) -> int:
+        if self.fa[x] != x:
+            self.fa[x] = self.find(self.fa[x])
+        return self.fa[x]
+    # 把 from 所在集合合并到 to 所在集合中
+    def merge(self, x: int, y: int) -> None:
+        x, y = self.find(x), self.find(y)
+        if x==y: return
+        self.fa[x] = y
+        self.cc -= 1
 
 sol = Solution()
 result = [
-
+    # sol.validateCoupons(code = ["SAVE20","","PHARMA5","SAVE@20"], businessLine = ["restaurant","grocery","pharmacy","restaurant"], isActive = [True,True,True,True]),
+    # sol.processQueries(c = 5, connections = [[1,2],[2,3],[3,4],[4,5]], queries = [[1,3],[2,1],[1,1],[2,2],[1,2]]),
+    # sol.processQueries(c = 3, connections = [], queries = [[1,1],[2,1],[1,1]]),
+    # sol.minTime(n = 2, edges = [[0,1,3]], k = 2),
+    # sol.minTime(3, [[2,0,4242],[2,1,7212]], 2),
+    sol.minMoves(sx = 1, sy = 2, tx = 5, ty = 4),
+    sol.minMoves(sx = 0, sy = 1, tx = 2, ty = 3),
+    sol.minMoves(sx = 1, sy = 1, tx = 2, ty = 2),
 ]
 for r in result:
     print(r)
